7a53d7806698799c0906c4637988a4550ae1e12f
Vladimir Komissarov <dr0id@dr0id.ru>
2012-09-27 01:05:11 +0700

    Add initial markups and styles

---
+++ src/Blog/FrontendBundle/Controller/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Blog\FrontendBundle\Controller;
+
+use Blog\DomainBundle\Service\PostService;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PostController extends Controller
+{
+    /**
+     * @Template()
+     * @return array
+     */
+    public function indexAction()
+    {
+        return array('posts' => $this->getPostService()->getAllPosts());
+    }
+
+    /**
+     * @Template()
+     * @param int $id
+     * @return array
+     */
+    public function viewAction($id)
+    {
+        return array('post' => $this->getPostService()->getPost($id));
+    }
+
+    /**
+     * @return PostService
+     */
+    private function getPostService()
+    {
+        return $this->get('domain.service.post');
+    }
+}
